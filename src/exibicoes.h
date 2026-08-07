a15df8c311e2d1642ad5c33a088e6cf29bd13b7b
Thiago César Rodrigues <thiacefilho@gmail.com>
2026-08-07 18:34:16 -0300

    feat: criar headers dos itens de exibicoes

---
+++ src/exibicoes.h
@@ -0,0 +1,12 @@
+// para listar desenhar e lidar com os itens da exposição
+
+#ifndef MUSEU_EXIBICOES_H
+#define MUSEU_EXIBICOES_H
+
+// inicia dados dos itens de exibiçoes (posições, cores, materiais)
+void exponatos_iniciar(void);
+
+// desenha todos os itens de exibiçoes da cena
+void exponatos_desenhar(void);
+
+#endif
