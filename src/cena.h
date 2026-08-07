953f63d29f308d70505dce0cb7f152848372c25e
Thiago César Rodrigues <thiacefilho@gmail.com>
2026-08-07 18:32:36 -0300

    feat: criar header para codigo da cena

---
+++ src/cena.h
@@ -0,0 +1,12 @@
+// geometria estatica da cena
+
+#ifndef MUSEU_CENA_H
+#define MUSEU_CENA_H
+
+// inicia os recursos da cena ( como carregar texturas de piso/parede)
+void cena_iniciar(void);
+
+// desenha a geometria estática (chão, parede, teto)
+void cena_desenhar(void);
+
+#endif 
